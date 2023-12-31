81bd94c029e92fd43cfa6c33293079845b936e67
GuiTsuke <guilhermebborges04m2@gmail.com>
2023-12-30 21:33:56 -0300

    Implementando o desafio de projeto da trilha do bootcamp Decola Tech 2024 e completando o desenvolvimento de smartphones

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone(numero: "556489", modelo: "Modelo 1", imei: "012345678910", memoria: 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n\nNokia:");
+Smartphone nokia = new Nokia(numero: "132498", modelo: "Modelo 2", imei: "109876543210", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
